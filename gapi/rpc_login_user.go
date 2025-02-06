1fd7f57768875c9b79fa803d4c6111f1598efea2
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-02-07 00:05:48 +0900

    implement gRPC service [skip ci]

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,72 @@
+package gapi
+
+import (
+	"context"
+	"errors"
+	db "simple-bank/db/sqlc"
+	"simple-bank/pb"
+	"simple-bank/util"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			return nil, status.Errorf(codes.NotFound, "user not found: %s", err)
+		}
+		return nil, status.Errorf(codes.Internal, "failed to find user: %s", err)
+	}
+
+	err = util.CheckPassword(req.GetPassword(), user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect password : %s", err)
+	}
+
+	accessToken, payload, err := server.tokenMaker.CreateToken(req.GetUsername(), server.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create access token: %s", err)
+	}
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(req.GetUsername(),
+		server.config.RefreshTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refreshToken: %s", err)
+	}
+	var pgUUID pgtype.UUID
+	err = pgUUID.Scan(refreshPayload.ID)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "scan uuid err: %s", err)
+	}
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           pgUUID,
+		Username:     refreshPayload.Username,
+		RefreshToken: refreshToken,
+		// UserAgent:    ctx.Request.UserAgent(),
+		// ClientIp:     ctx.ClientIP(),
+		UserAgent: "",
+		ClientIp:  "",
+		IsBlocked: false,
+		ExpiresAt: pgtype.Timestamptz{
+			Time:  refreshPayload.ExpiresAt.Time,
+			Valid: true,
+		},
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "create session error: %s", err)
+	}
+
+	rsp := &pb.LoginUserResponse{
+		User:                  convertUser(user),
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		RefreshToken:          refreshToken,
+		AccessTokenExpiresAt:  timestamppb.New(payload.ExpiresAt.Time),
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiresAt.Time),
+	}
+
+	return rsp, nil
+}
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"log"
+	"net"
 	"simple-bank/api"
 	db "simple-bank/db/sqlc"
+	"simple-bank/gapi"
+	"simple-bank/pb"
 	"simple-bank/util"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -20,11 +25,35 @@ func main() {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	testStore := db.NewStore(pool)
+	runGrpcServer(config, testStore)
+}
+
+func runGrpcServer(config util.Config, testStore db.Store) {
+	server, err := gapi.NewServer(config, testStore)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GrpcServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, testStore db.Store) {
 	server, err := api.NewServer(config, testStore)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
