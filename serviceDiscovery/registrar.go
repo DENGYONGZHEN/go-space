f68c56a6de3c48e13bbc9fb8dee52e1c6a4c0dd5
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-03-15 18:46:48 +0900

    a simple service discovery

---
+++ serviceDiscovery/registrar.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/events"
+	"github.com/docker/docker/api/types/filters"
+)
+
+type Registrar struct {
+	DockerClient *DockerClient
+	SRegistry    *ServiceRegistry
+}
+
+func (r *Registrar) Init() error {
+	cList, err := r.DockerClient.ContainerList(context.Background(), container.ListOptions{
+		Filters: filters.NewArgs(
+			filters.Arg("ancestor", HelloServiceImageName),
+			filters.Arg("status", ContainerRunningState),
+		),
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, c := range cList {
+		r.SRegistry.Add(c.ID, findContainerAddress(c.Ports[0].PublicPort))
+	}
+	return nil
+}
+
+func (r *Registrar) Observe() {
+	msgCh, errCh := r.DockerClient.Events(context.Background(), events.ListOptions{
+		Filters: filters.NewArgs(
+			filters.Arg("type", "container"),
+			filters.Arg("image", HelloServiceImageName),
+			filters.Arg("event", "start"),
+			filters.Arg("event", "kill"),
+		),
+	})
+
+	for {
+		select {
+		case c := <-msgCh:
+			fmt.Printf("State of the contianer %s is %s\n", c.ID, c.Status)
+			if c.Status == ContainerKillState {
+				r.SRegistry.RemoveByContainerID(c.ID)
+			} else if c.Status == ContainerStartState {
+				port, err := r.DockerClient.GetContainerPort(context.Background(), c.ID)
+				if err != nil {
+					fmt.Printf("err geeting newly started container port %s\n", err.Error())
+					continue
+				}
+				r.SRegistry.Add(c.ID, findContainerAddress(port))
+			}
+		case err := <-errCh:
+			fmt.Println("Error Docker Event Chan", err.Error())
+		}
+	}
+}
+
+func findContainerAddress(cPort uint16) string {
+	return fmt.Sprintf("http://localhost:%d", cPort)
+}
