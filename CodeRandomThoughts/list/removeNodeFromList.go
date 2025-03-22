577ed252cf8f0a412ac7c10fc3928ceaa9c07c60
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-03-22 22:57:10 +0900

    remove element from linked list

--- CodeRandomThoughts/list/removeNodeFromList.go
+++ CodeRandomThoughts/list/removeNodeFromList.go
@@ -1,10 +1,44 @@
 package list
 
-type Node struct {
-	value int
-	next  *Node
+//Given the head of a linked list and an integer val,
+// remove all the nodes of the linked list that has Node.val == val,
+// and return the new head.
+
+// Example 1:
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+
+// Example 2:
+// Input: head = [], val = 1
+// Output: []
+
+// Example 3:
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func removeNodeFromList() {
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	dummyHead := &ListNode{
+		Next: head,
+	}
+	current := dummyHead
+
+	for current.Next != nil {
+
+		if current.Next.Val == val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return dummyHead.Next
 
 }
--- CodeRandomThoughts/list/removeNodeFromList_test.go
+++ CodeRandomThoughts/list/removeNodeFromList_test.go
@@ -1,1 +1,63 @@
 package list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveElements(t *testing.T) {
+
+	testCases := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{
+			nums:   []int{1, 2, 6, 3, 4, 5, 6},
+			target: 6,
+			want:   []int{1, 2, 3, 4, 5},
+		},
+		{
+			nums:   []int{},
+			target: 1,
+			want:   []int{},
+		},
+		{
+			nums:   []int{7, 7, 7, 7},
+			target: 7,
+			want:   []int{},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		if len(tc.nums) == 0 {
+			val := removeElements(nil, tc.target)
+			if val != nil {
+				t.Errorf(" removeElements(nil,%d),should get %v, but get %v", tc.target, tc.want, val)
+			}
+		}
+		head := &ListNode{}
+		current := head
+
+		for _, v := range tc.nums {
+			current.Next = &ListNode{
+				Val: v,
+			}
+			current = current.Next
+		}
+		val := removeElements(head.Next, tc.target)
+		current.Next = val
+		result := []int{}
+
+		for current.Next != nil {
+			result = append(result, current.Next.Val)
+			current = current.Next
+		}
+
+		if !reflect.DeepEqual(result, tc.want) {
+			t.Errorf(" removeElements(nil,%d),should get %v, but get %v", tc.target, tc.want, result)
+		}
+
+	}
+}
