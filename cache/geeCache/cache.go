089bb7cb91aa7907d70e03046d5169e9db5c9cc7
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-04-03 23:08:39 +0900

    cache server

--- cache/geeCache/cache.go
+++ cache/geeCache/cache.go
@@ -3,7 +3,7 @@ package geecache
 import (
 	"sync"
 
-	"github.com/deng/go-space/little-practice/geecache/lru"
+	"github.com/deng/go-space/little-practice/geecache/geeCache/lru"
 )
 
 type cache struct {
