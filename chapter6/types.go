a2a771b584a56d4ec8eb55da0b638699b8ef5f57
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-07-08 23:59:50 +0900

    udp

---
+++ chapter6/types.go
@@ -0,0 +1,34 @@
+package chapter6
+
+const (
+	DatagramSize = 516              //the maximum supported datagram size
+	BlockSize    = DatagramSize - 4 // the DatagramSize minus a 4 byte header
+)
+
+type OpCode uint16
+
+const (
+	OpRRQ OpCode = iota + 1
+	_            // no WRQ support
+	OpData
+	OpAck
+	OpErr
+)
+
+type ErrCode uint16
+
+const (
+	ErrUnknown ErrCode = iota
+	ErrNotFound
+	ErrAccessViolation
+	ErrDiskFull
+	ErrIllegalOp
+	ErrUnknownID
+	ErrFileExists
+	ErrNoUser
+)
+
+type ReadReq struct {
+	Filename string
+	Mode     string
+}
