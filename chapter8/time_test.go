077547550da31ad6c41ce4457b69822481ee4f0f
DENG YONGZHEN <yongzhendeng111@gmail.com>
2025-07-16 00:05:01 +0900

    http

--- chapter8/time_test.go
+++ chapter8/time_test.go
@@ -12,7 +12,13 @@ func TestHeadTime(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_ = resp.Body.Close()
+	//如果不关闭 response.Body，Go 的 HTTP 客户端 无法重用连接，并且会 泄露文件描述符，最终可能导致后续请求失败。
+	// 当你调用 client.Do(...) 后，会打开一个文件描述符（例如 socket）。若不调用 resp.Body.Close()，这个资源不会被释放，会一直占用。
+
+	//defer resp.Body.Close()
+
+	//io.Copy(io.Discard, resp.Body)  // 可选：读取并丢弃，确保重用连接
+	_ = resp.Body.Close() // 无论是否读取，都要关闭
 	now := time.Now().Round(time.Second)
 	date := resp.Header.Get("Date")
 	if date == "" {
